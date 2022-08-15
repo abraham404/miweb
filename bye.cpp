226aa376b71307d258b9cb87df0396e15aea6ad1
Abraham Rosas <abraham.work7@gmail.com>
2022-08-14 20:35:51 -0600

    primeros pasos

---
+++ bye.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+
+int main(){
+
+    cout<<"bye Abraham"<<endl;
+
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+
+
+int main(){
+
+    cout<<"Hola"<<endl;
+
+    return 0;
+}
